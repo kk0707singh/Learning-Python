603159fb8bff9ae1a5ac836f40bb746c70a23902
kk0707singh <kk0707singh@gmail.com>
2026-08-05 17:30:02 +0530

    modules-packages

--- Chapter-08-Functions-Recursions/01functions.py
+++ Chapter-08-Functions-Recursions/01functions.py
@@ -41,3 +41,19 @@ avg()       #function call
 print("I have just started function ")  
 avg()
 avg()
+
+# lambda function
+# syntax to write lambda function
+# lambda arguments: expression
+addition = lambda a, b: a+b
+type(addition)
+print(addition(2, 5))
+
+# basic functions:
+def addition1(a, b):
+    return a+b
+print(addition1(2, 5))
+
+
+# lambda function for even odd:
+even = lambda num:num%2==0
