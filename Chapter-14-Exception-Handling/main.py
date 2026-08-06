b8bc9e5b2e75458a59a9c711184190ff3342a557
kk0707singh <kk0707singh@gmail.com>
2026-08-06 16:01:20 +0530

    python completed!

--- Chapter-14-Exception-Handling/main.py
+++ Chapter-14-Exception-Handling/main.py
@@ -1,53 +1,53 @@
 # a = b
 # output:- NameError: name 'b' is not defined
 # how to handle the exception for the above using try, block:
-# try:
-#     a = b
-# except:
-#     print("the variable has not been assigned yet!")
+try:
+    a = b
+except:
+    print("the variable has not been assigned yet!")
 
 
-# # other ways to handle the exception:
-# try: 
-#     a = b
-# except NameError as ne:
-#     print(ne)
+# other ways to handle the exception:
+try: 
+    a = b
+except NameError as ne:
+    print(ne)
 
 
-# # other example to handle the exception:
-# try:
-#     result = 1/0
-# except ZeroDivisionError as zde:
-#     print(zde)
-#     print('Please enter the denominator greator than ZERO!')
+# other example to handle the exception:
+try:
+    result = 1/0
+except ZeroDivisionError as zde:
+    print(zde)
+    print('Please enter the denominator greator than ZERO!')
 
 
-# # how to handle any exception when we dont know the error class:
-# try:
-#     result = 1/2
-#     a = b
-# except ZeroDivisionError as zde:
-#     print(zde)
-# except Exception as ex:
-#     print(ex)
+# how to handle any exception when we dont know the error class:
+try:
+    result = 1/2
+    a = b
+except ZeroDivisionError as zde:
+    print(zde)
+except Exception as ex:
+    print(ex)
 
 
 # example:
-# try:
-#     num = int(input("Enter the no: "))
-#     result = 100/num
-#     print(result)
-# except NameError as ne1:
-#     print(ne1)
-#     print("Enter a valid no! ")
-# except ValueError as ve:
-#     print(ve)
-#     print('not a valid no! ')
-# except ZeroDivisionError as zde1:
-#     print(zde1)
-#     print('enter the no greater than ZERO! ')
-# except Exception as ex1:
-#     print(ex1)
+try:
+    num = int(input("Enter the no: "))
+    result = 100/num
+    print(result)
+except NameError as ne1:
+    print(ne1)
+    print("Enter a valid no! ")
+except ValueError as ve:
+    print(ve)
+    print('not a valid no! ')
+except ZeroDivisionError as zde1:
+    print(zde1)
+    print('enter the no greater than ZERO! ')
+except Exception as ex1:
+    print(ex1)
 
 
 # try, except, else and finally:
